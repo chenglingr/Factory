7474a1dd576e9b5c97db657b2440b5a620d347f6
chenglingr <286863096@qq.com>
2016-05-04 15:41:15 +0800

    cf

---
+++ web/Admin/ashx/Del.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Admin.ashx
+{
+    /// <summary>
+    /// Del 的摘要说明
+    /// </summary>
+    public class Del : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string json = "{}";
+            string action = context.Request.Form["Action"];
+            if (action == "Del")//删除操作
+            {
+                string DelNumS = context.Request.Form["DelNumS"];//获取批量删除的编号
+                BLL.Admin bll = new BLL.Admin();
+                if (bll.DeleteList(DelNumS))
+                {
+                    json = "{'info':'删除成功'}";
+                }
+                else
+                { json = "{'info':'删除失败'}"; }
+            }
+            context.Response.Write(json);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
